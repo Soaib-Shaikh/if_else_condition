48c51120e46e724104e1317a0326457b14faa3f8
Soaib Shaikh <samshaikh1192@gmail.com>
2025-01-11 09:49:47 -0800

    change structure

--- web_dsa_structure/prog1.cpp
+++ web_dsa_structure/prog1.cpp
@@ -3,11 +3,15 @@ using namespace std;
 
 int main()
 {
-    int num=10;
+    int num= 10;
 
-    if (num=10)
+    if (num > 0)
     {
         cout <<"Hello cpp programers.";
+    }
+    else 
+    {
+        cout <<"Hii cpp programers .";
     }
        
     return 0;
